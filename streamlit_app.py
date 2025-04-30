fec49a6d82b801509cce2abad518c8d26289931a
bulanoX <gavasblack@gmail.com>
2025-04-30 13:24:28 +0300

    Small change to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,6 @@ if ingredients_list and len(ingredients_list)<6:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+
+
