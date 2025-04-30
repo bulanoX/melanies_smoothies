2ca1b56d2df3fbd1ff1a54318389ae1bc4e3b2ce
bulanoX <gavasblack@gmail.com>
2025-04-30 14:02:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
+
 name_on_order=st.text_input('Name on Smoothie')
 st.write("The name on your Smoothie will be:",name_on_order)
 
