338f8ff500137a4840cc4a1e8aad7adb191e8051
bulanoX <gavasblack@gmail.com>
2025-04-30 13:27:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ if ingredients_list and len(ingredients_list)<6:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
